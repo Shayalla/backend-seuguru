89b7df209a316675307cd80db8cff25b214d040b
Jessie <miss_sc_jessi@hotmail.com>
2021-12-01 20:22:46 -0300

    agora retorna um message com o resultado da requisicao

--- controllers/coverageController.js
+++ controllers/coverageController.js
@@ -4,7 +4,7 @@ const { createCoverage } = require('../services/coverageService');
 const newCoverage = rescue(async (req, res) => {
   const { insurance_id, name, factor } = req.body;
   const coverage = await createCoverage({ insurance_id, name, factor });
-  return res.status(coverage.code).json(coverage.result);
+  return res.status(coverage.code).json({ message: coverage.result });
 });
 
 module.exports = { newCoverage };
--- controllers/insuranceController.js
+++ controllers/insuranceController.js
@@ -6,12 +6,12 @@ const { simulator } = require('../services/coverageService');
 const newInsurance = rescue(async (req, res) => {
   const { name } = req.body;
   const insurance = await createInsurance({ name });
-  return res.status(insurance.code).json(insurance.result);
+  return res.status(insurance.code).json({ message: insurance.result });
 });
 
 const getInsurances = rescue(async (_req, res) => {
   const insurances = await getAllInsurances();
-  res.status(insurances.code).json(insurances.result);
+  res.status(insurances.code).json({ message: insurances.result });
 });
 
 const simulatorInsurance = rescue(async (req, res) => {
@@ -19,7 +19,7 @@ const simulatorInsurance = rescue(async (req, res) => {
   const { coverage_id, capital } = coberturas[0];
   const simulatorUser = await simulator({ insurance_id, coverage_id, capital });
 
-  res.status(simulatorUser.code).json(simulatorUser.result);
+  res.status(simulatorUser.code).json({ message: simulatorUser.result });
 });
 
 module.exports = {
