0d11b41dcf4974844285e5b13d520832a5d73049
Jessie <miss_sc_jessi@hotmail.com>
2021-12-01 11:23:54 -0300

    arrumado identacao

--- controllers/insuranceController.js
+++ controllers/insuranceController.js
@@ -2,9 +2,9 @@ const rescue = require('express-rescue');
 const { createInsurance } = require('../services/insuranceService');
 
 const newInsurance = rescue(async(req, res) => {
-    const { name } = req.body;
-    const insurance = await createInsurance({ name });
-    return res.status(insurance.code).json(insurance.result);
-  });
+  const { name } = req.body;
+  const insurance = await createInsurance({ name });
+  return res.status(insurance.code).json(insurance.result);
+});
 
 module.exports = { newInsurance };
